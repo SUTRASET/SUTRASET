043a63ed08fba90c5ea48c72788466a7f20eaaad
Chenming <cmzhang1318@hotmail.com>
2015-09-28 14:03:47 +1000

    one fujimaki case is added

---
+++ examples/Fujimaki/Toyoura/sl_read.m
@@ -0,0 +1,22 @@
+clear all
+close all
+% project name
+name='Column';
+
+% read input files
+%inp=i0('desaline');
+inp  = inpObj(  name);
+nod  = readNOD( name);
+ele  = readELE( name);
+bcop = readBCOP(name);
+bcof = readBCOF(name);
+
+% index for p c and s
+x_idx  = strcmp(nod(1).label,'X');
+y_idx  = strcmp(nod(1).label,'Y');
+p_idx  = strcmp(nod(1).label,'Pressure');
+c_idx  = strcmp(nod(1).label,'Concentration');
+s_idx  = strcmp(nod(1).label,'Saturation');
+sm_idx = strcmp(nod(1).label,'SM');
+vy_idx = strcmp(ele(1).label,'Y velocity');
+yele_idx = strcmp(ele(1).label,'Y origin');
